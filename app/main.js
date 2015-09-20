813034932493098af7a7af2e77508345c59db694
Vladimir Bauer <vbauerster@gmail.com>
2015-09-20 20:52:33 +0300

    playground

---
+++ app/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var HelloMessage = React.createClass({
+	render: function() {
+		return <div>Hello {this.props.name}</div>;
+	}
+});
+
+React.render(<HelloMessage name="John" />, document.getElementById('app'));
